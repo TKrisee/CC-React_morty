69e43f2379fc981f26dcf273270c90af520f64a1
TOM-ATIV9\Tom <thomyerk@gmail.com>
2021-03-16 15:15:15 +0100

    Updates

---
+++ precache-manifest.0c7569df1f2acf71b0eb21ac622fef14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad9e2e65d2cb8f181ec66d41f6aab78",
+    "url": "/CC-React_morty/index.html"
+  },
+  {
+    "revision": "03f5d73db2106f33868f",
+    "url": "/CC-React_morty/static/css/main.c99f0a59.chunk.css"
+  },
+  {
+    "revision": "df9b36ac9c74f24c55d3",
+    "url": "/CC-React_morty/static/js/2.2169a68e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CC-React_morty/static/js/2.2169a68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f5d73db2106f33868f",
+    "url": "/CC-React_morty/static/js/main.015d2b2c.chunk.js"
+  },
+  {
+    "revision": "9afa5e5ec57d5f3e6e53",
+    "url": "/CC-React_morty/static/js/runtime-main.a9d5ea35.js"
+  },
+  {
+    "revision": "6950e761b3c9c144b1a44f444df631d5",
+    "url": "/CC-React_morty/static/media/backgroundPic.6950e761.png"
+  }
+]);
